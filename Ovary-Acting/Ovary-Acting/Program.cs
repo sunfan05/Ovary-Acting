fc707447f2d1f773b03a619bb935bf950b5e23bc
sunfan05 <84829334+sunfan05@users.noreply.github.com>
2021-11-27 20:52:45 +0800

    Add files via upload

---
+++ Ovary-Acting/Ovary-Acting/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace OvaryActing
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //starting page
+            //add ascii art & "loading . . ."
+            //"Press enter to continue"
+
+
+            //start of game
+            int DayNumber = 0;
+            while (DayNumber < 5)
+            {
+                DayNumber += 1;
+                Console.WriteLine("[Day " + DayNumber + " of Period]");
+                Console.WriteLine();
+
+                //START selecting the right pad minigame
+                SelectPad minigame1 = new SelectPad();
+                minigame1.selectpad();
+
+
+                //laundry minigame --->> PROBLEM: repeats questions
+
+
+                //START hot or cold compress minigame
+
+                if (DayNumber != 5)
+                {
+                    endmsg1();
+                }
+                else if (DayNumber == 5)
+                {
+                    endmsg2();
+                    break;
+                }
+            }
+        }
+        public static void endmsg1()
+        {
+            Console.WriteLine("Congrats, you got through the day! See u tomorrow :))");
+            Console.WriteLine("Press Enter to continue to the next day");
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+        public static void endmsg2()
+        {
+            Console.WriteLine("Thanks for playing, see you next month! (or in approximately 28 days)");
+            Console.WriteLine("Press Enter to end the game");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ Ovary-Acting/Ovary-Acting/SelectPad.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace OvaryActing
+{
+    public class SelectPad
+    {
+        public void selectpad()
+        {
+            int RandomNumber = new Random().Next(1, 3);
+            string A = "〚A〛Day pad";
+            string B = "〚B〛Night pad";
+            string Answer;
+            if (RandomNumber == 1)
+            {
+                Console.WriteLine("It is daytime! Select the right pad");
+                Console.WriteLine(A);
+                Console.WriteLine(B);
+
+                Console.Write("Answer: ");
+                Answer = Console.ReadLine();
+                Console.WriteLine();
+
+                if (Answer == "A") {
+                    Console.WriteLine("Congrats, you're right!");
+                    Console.WriteLine("You may now proceed to the next minigame");
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Compress minigame3 = new Compress();
+                    minigame3.compress("start");
+                }
+                else
+                {
+                    Console.WriteLine("Oh no! The answer is supposed to be" + A + ".");
+                    Console.WriteLine("It’s now time for you to do the laundry.");
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    Laundry minigame2 = new Laundry();
+                    minigame2.laundry("start"); 
+                }
+
+            }
+            else if (RandomNumber == 2)
+            {
+                Console.WriteLine("It is nighttime! Select the right pad");
+                Console.WriteLine(A);
+                Console.WriteLine(B);
+
+                Console.Write("Answer: ");
+                Answer = Console.ReadLine();
+                Console.WriteLine();
+                if (Answer == "B")
+                {
+                    Console.WriteLine("Congrats, you're right!");
+                    Console.WriteLine("You may now proceed to the next minigame");
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Compress minigame3 = new Compress();
+                    minigame3.compress("start");
+                }
+                else
+                {
+                    Console.WriteLine("Oh no! The answer is supposed to be" + B + ".");
+                    Console.WriteLine("It’s now time for you to do the laundry.");
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    Laundry minigame2 = new Laundry();
+                    minigame2.laundry("start");
+                }
+            }
+            return;
+        }
+    }
+}
