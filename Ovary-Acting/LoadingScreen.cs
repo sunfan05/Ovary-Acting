3b4b15664361bce471f1579394d8e0db75720ada
sunfan05 <84829334+sunfan05@users.noreply.github.com>
2021-12-01 15:58:33 +0800

    Add files via upload

---
+++ Ovary-Acting/LoadingScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace OvaryActing
+{
+    public class LoadingScreen
+    {
+        public void loadingscreen()
+        { 
+            Console.WriteLine("        Ovary-Acting         ");
+            Console.WriteLine("          ........           ");
+            Console.WriteLine("        ..        .          ");
+            Console.WriteLine("        .          .         ");
+            Console.WriteLine("        .          .         ");
+            Console.WriteLine("        .          .         ");
+            Console.WriteLine("       ...        ...        ");
+            Console.WriteLine("      ....        ....       ");
+            Console.WriteLine("      ....        ....       ");
+            Console.WriteLine("       ...        ...        ");
+            Console.WriteLine("        .         ..         ");
+            Console.WriteLine("        .          .         ");
+            Console.WriteLine("        .          .         ");
+            Console.WriteLine("        .         ..         ");
+            Console.WriteLine("         ..      ..          ");
+            Console.WriteLine("           .....             ");
+            Console.WriteLine();
+            Console.WriteLine("  By: Anika, Jimin, Vincent  ");
+            Console.WriteLine();
+            Console.WriteLine("    Press Enter to Begin     ");
+            Console.ReadLine();
+            Console.Clear();
+            return;
+          
+        }
+    }
+}
