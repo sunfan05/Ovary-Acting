531880866fe4bbbf922ac9ea99ec48d9dc3dad82
sunfan05 <84829334+sunfan05@users.noreply.github.com>
2021-12-04 13:02:47 +0800

    Add files via upload

---
+++ Ovary-Acting/Ovary-Acting/AdditionalMinigame.cs
@@ -0,0 +1,331 @@
+﻿using System;
+namespace OvaryActing
+{
+    public class AdditionalMinigame
+    {
+        public void additionalminigame()
+        {
+            //introduction
+            Console.WriteLine("[Day " + DayNumber.day + " of Period]");
+            Console.WriteLine();
+            Console.WriteLine("❮Current Health: " + HP.result + "❯");
+            Console.WriteLine();
+            Console.WriteLine("Well.. you weren't able to get 100 points to get through the day (｡•́︿•̀｡)");
+            Console.WriteLine("- You now have to go through a series of questions to get your HP to 100 points.");
+            Console.WriteLine("- Each correct question is worth 25 points so answer as many as you can correctly!");
+            Console.WriteLine("- But take note, if you get a question wrong, you will lose 25 points.");
+            Console.WriteLine("- Some questions may have more than one answer. You may just type in any one of these answers");
+            Console.WriteLine("- Also, some questions may be repeated.");
+            Console.WriteLine();
+            Console.WriteLine("Are you ready to start? Press Enter to begin!!");
+            Console.ReadLine();
+            Console.Clear();
+            while (HP.result < 100)
+            {
+                Console.WriteLine("[Day " + DayNumber.day + " of Period]");
+                Console.WriteLine();
+                int n = new Random().Next(1, 11);
+                string Answer;
+                Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                Console.WriteLine();
+                //questions
+
+                if (n == 1)
+                {
+                    Console.WriteLine("What do u do when u see a girl with blood on her clothes?");
+                    Console.WriteLine("〚A〛Laugh");
+                    Console.WriteLine("〚B〛Call the cops");
+                    Console.WriteLine("〚C〛Tell her and give her something to cover up");
+                    Console.WriteLine("〚D〛Run away. It's gross");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if (Answer == "C" || Answer == "c")
+                    {
+                        Console.WriteLine("Nice one! You're correct o(>ω<)o");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (;ω;). The correct answer is〚C〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 2)
+                {
+                    Console.WriteLine("Your friend asks to borrow money to buy pads. What do you do?");
+                    Console.WriteLine("〚A〛Tell her to just hold it in until she gets home. ");
+                    Console.WriteLine("〚B〛Give her a tissue. It’s the same thing, right?");
+                    Console.WriteLine("〚C〛Lend her the money. She needs the pads urgently. ");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if (Answer == "C" || Answer == "c")
+                    {
+                        Console.WriteLine("Nice one! You're correct o(>ω<)o");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (;ω;). The correct answer is〚C〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 3)
+                {
+                    Console.WriteLine("You’ve just gotten your first period while at school, and you don’t have a pad! Who do you tell? ");
+                    Console.WriteLine("〚A〛Your best friend, maybe you can figure this out together.");
+                    Console.WriteLine("〚B〛Your teacher, maybe they can help you find pads.");
+                    Console.WriteLine("〚C〛Call your mom, maybe she can bring you home.");
+                    Console.WriteLine("〚D〛Go to the school clinic and explain.");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if ((Answer == "B" || Answer == "b") || (Answer == "C" || Answer == "c") || (Answer == "D" || Answer == "d"))
+                    {
+                        Console.WriteLine("Nice one! You're correct o(>ω<)o. The correct answers are〚B〛,〚C〛,〚D〛");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (;ω;). The correct answers are〚B〛,〚C〛,〚D〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 4)
+                {
+                    Console.WriteLine("Which of these establishments probably sells pads?");
+                    Console.WriteLine("〚A〛Pan de Manila. More like pad de Manila");
+                    Console.WriteLine("〚B〛Mercury Drugstore. They also sell diapers.");
+                    Console.WriteLine("〚C〛GNC. They sell health stuff.");
+                    Console.WriteLine("〚D〛Jollibee. The bee is red.");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if (Answer == "B" || Answer == "b")
+                    {
+                        Console.WriteLine("Nice one! You're correct ヽ(・∀・)ﾉ");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (╯︵╰,). The correct answer is〚B〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 5)
+                {
+                    Console.WriteLine("You leaked blood on your bedsheets. What do you do?");
+                    Console.WriteLine("〚A〛Launder the whole bedsheet");
+                    Console.WriteLine("〚B〛Rub the stain with hydrogen peroxide");
+                    Console.WriteLine("〚C〛Rub the stain with dishwashing liquid");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if ((Answer == "A" || Answer == "a") || (Answer == "B" || Answer == "b") || (Answer == "C" || Answer == "c"))
+                    {
+                        Console.WriteLine("Nice one! You're correct ヽ(・∀・)ﾉ. Actually, all the choices above are correct!");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (╯︵╰,). The correct answers are〚A〛,〚B〛,〚C〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 6)
+                {
+                    Console.WriteLine("Who should learn about periods?");
+                    Console.WriteLine("〚A〛Only people who are female, since only they have to deal with it.");
+                    Console.WriteLine("〚B〛Everyone. We need to look out for each other.");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if (Answer == "B" || Answer == "b")
+                    {
+                        Console.WriteLine("Nice one! You're correct (✯◡✯)");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (╯︵╰,). The correct answer is〚B〛");
+                        Console.WriteLine("You lost -5HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 7)
+                {
+                    Console.WriteLine("Who can experience menstruation?");
+                    Console.WriteLine("〚A〛Only women");
+                    Console.WriteLine("〚B〛All women");
+                    Console.WriteLine("〚C〛Anyone");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if (Answer == "C" || Answer == "c")
+                    {
+                        Console.WriteLine("Nice one! You're correct (✯◡✯)");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (｡•́︿•̀｡). The correct answer is〚C〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 8)
+                {
+                    Console.WriteLine("How long should you use the same pad?");
+                    Console.WriteLine("〚A〛One pad will last the whole cycle");
+                    Console.WriteLine("〚B〛Change it every hour");
+                    Console.WriteLine("〚C〛Change it every four hours");
+                    Console.WriteLine("〚D〛Change it every day");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if ((Answer == "C" || Answer == "c") || (Answer == "B" || Answer == "b"))
+                    {
+                        Console.WriteLine("Nice one! You're correct (✯◡✯). The correct answers are〚B〛,〚C〛");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (｡•́︿•̀｡). The correct answers are〚B〛,〚C〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 9)
+                {
+                    Console.WriteLine("Do all Philippine schools provide free pads to students?");
+                    Console.WriteLine("〚A〛Yes, because some students may not have access to it otherwise");
+                    Console.WriteLine("〚B〛Yes, because it is as necessary as toilet paper");
+                    Console.WriteLine("〚C〛Yes, but only private schools");
+                    Console.WriteLine("〚D〛No");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if (Answer == "D" || Answer == "d")
+                    {
+                        Console.WriteLine("Nice one! You're correct <(￣︶￣)>.");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (｡•́︿•̀｡). The correct answer is〚D〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                else if (n == 10)
+                {
+                    Console.WriteLine("What are the wings on pads for?");
+                    Console.WriteLine("〚A〛So the period will fly away");
+                    Console.WriteLine("〚B〛To make the pad lighter");
+                    Console.WriteLine("〚C〛To secure the pad in place");
+                    Console.WriteLine("〚D〛For aesthetic purposes");
+                    Console.WriteLine();
+                    Console.Write("Answer: ");
+                    Answer = Console.ReadLine();
+                    Console.WriteLine();
+                    if (Answer == "C" || Answer == "c")
+                    {
+                        Console.WriteLine("Nice one! You're correct <(￣︶￣)>.");
+                        Console.WriteLine("Have +25HP!");
+                        HP.result += 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is wrong (｡•́︿•̀｡). The correct answer is〚C〛");
+                        Console.WriteLine("You lost -25HP!");
+                        HP.result -= 25;
+                        Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ Ovary-Acting/Ovary-Acting/SelectPad.cs
@@ -0,0 +1,149 @@
+﻿using System;
+namespace OvaryActing
+{
+    public class SelectPad
+    {
+        public void selectpad()
+        {
+            Console.WriteLine("❮Current Health: " + HP.result + "❯");
+            int RandomNumber = new Random().Next(1, 3);
+            string A = "〚A〛Day pad";
+            string B = "〚B〛Night pad";
+            string Answer;
+            Console.WriteLine();
+
+            if (RandomNumber == 1)
+            {
+                Console.WriteLine("Rise and Shine!"); 
+                Console.WriteLine("            .:                   ");
+                Console.WriteLine("        :   :-   -               ");
+                Console.WriteLine("   .-.  .-  :-  -.   ::          ");
+                Console.WriteLine("    .--. ....:...  :-.           ");
+                Console.WriteLine("  :.  ..:-======-:..  .:.        ");
+                Console.WriteLine("   .:: -==========- .:.          ");
+                Console.WriteLine("..... :============- .....       ");
+                Console.WriteLine("..... :============- .....       ");
+                Console.WriteLine("   .:: -==========-..:.          ");
+                Console.WriteLine(" .:.   .:-======-:.   .:.        ");
+                Console.WriteLine("     :-. ....:... .--.           ");
+                Console.WriteLine("   .-.  .-  :-  -:  .-:          ");
+                Console.WriteLine("        :   :-   -               ");
+                Console.WriteLine("            .:                   ");
+                Console.WriteLine();
+                Console.WriteLine("It is daytime! Select the right pad");
+                Console.WriteLine(A);
+                Console.WriteLine(B);
+
+                Console.WriteLine();
+                Console.Write("Answer: ");
+                Answer = Console.ReadLine();
+                Console.WriteLine();
+                Console.Clear();
+                Console.WriteLine("[Day " + DayNumber.day + " of Period]");
+                Console.WriteLine();
+
+                if (Answer == "A" || Answer == "a")
+                {
+                    HP.result += 50;
+                    Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    Console.WriteLine();
+                    Console.WriteLine("Congrats, you're right! Have +50HP");
+                    Console.WriteLine();
+                    Console.WriteLine("But..");
+                    Console.WriteLine("(Press Enter to see the rest of the text ┐(︶▽︶)┌)");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Compress minigame3 = new Compress();
+                    minigame3.compress("start");
+                }
+                else
+                {
+                    HP.result -= 50;
+                    Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Oh no!");
+                    Console.ResetColor();
+                    Console.WriteLine("The answer is supposed to be" + A + ".");
+                    Console.WriteLine("You lost -50HP :(");
+                    Console.WriteLine();
+                    Console.WriteLine("Pssst... Guess what?");
+                    Console.WriteLine("(Press Enter if you're curious ᕕ( ᐛ )ᕗ)");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    Laundry minigame2 = new Laundry();
+                    minigame2.laundry("start");
+                }
+
+            }
+            else if (RandomNumber == 2)
+            {
+                Console.WriteLine("Good evening!");
+                Console.WriteLine("         ....                  ");
+                Console.WriteLine("      .::-:.                   ");
+                Console.WriteLine("    .:---.                     ");
+                Console.WriteLine("   :----.                      ");
+                Console.WriteLine("  :----:                       ");
+                Console.WriteLine(" :::---.                       ");
+                Console.WriteLine(".:::---.                       ");
+                Console.WriteLine(".::::---                       ");
+                Console.WriteLine(".::::---:                    . ");
+                Console.WriteLine(" :::::----                  -. ");
+                Console.WriteLine("  ::::------.            .-+:  ");
+                Console.WriteLine("   ::::----===--::..::-=+++:   ");
+                Console.WriteLine("    :------=========+++++-     ");
+                Console.WriteLine("      .---==========++=:       ");
+                Console.WriteLine("         .::-------:.          ");
+                Console.WriteLine();
+                Console.WriteLine("It is nighttime! Select the right pad");
+       
+                Console.WriteLine(A);
+                Console.WriteLine(B);
+
+                Console.WriteLine();
+                Console.Write("Answer: ");
+                Answer = Console.ReadLine();
+                Console.WriteLine();
+                Console.Clear();
+                Console.WriteLine("[Day " + DayNumber.day + " of Period]");
+                Console.WriteLine();
+                if (Answer == "B"|| Answer == "b")
+                {
+                    HP.result += 50;
+                    Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    Console.WriteLine();
+                    Console.WriteLine("Congrats, you're right! Have +50HP");
+                    Console.WriteLine();
+                    Console.WriteLine("But..");
+                    Console.WriteLine("(Press Enter to see the rest of the text ┐(︶▽︶)┌)");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Compress minigame3 = new Compress();
+                    minigame3.compress("start");
+                }
+                else
+                {
+                    HP.result -= 50;
+                    Console.WriteLine("❮Current Health: " + HP.result + "❯");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Oh no, You leaked!");
+                    Console.ResetColor();
+                    Console.WriteLine("The answer is supposed to be" + B + ".");
+                    Console.WriteLine("You lost -50HP :(");
+                    Console.WriteLine();
+                    Console.WriteLine("Pssst... Guess what?");
+                    Console.WriteLine("(Press Enter if you're curious ᕕ( ᐛ )ᕗ)");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    Laundry minigame2 = new Laundry();
+                    minigame2.laundry("start");
+                }
+            }
+            return;
+        }
+    }
+
+}
